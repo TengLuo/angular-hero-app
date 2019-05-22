0f7c983945580d2b7a1b7f87ee2841b5efc264ad
TengLuo <luoteng91@gmail.com>
2019-05-22 01:12:55 -0400

    Create dashboard component and setup router

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
+import { TestpageComponent } from './testpage/testpage.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: 'heroes', component: HeroesComponent}
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'test', component: TestpageComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', redirectTo:'/dashboard',pathMatch:'full'}
 ];
 
 @NgModule({
--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TestpageComponent } from './testpage/testpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent,
+    TestpageComponent
   ],
   imports: [
     BrowserModule,
